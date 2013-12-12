67783345833350dea4421649bb8e5f8b6fd12190
mhemeryck <martijn.hemeryck@soundtalks.com>
2013-12-12 08:59:38 +0100

    removed bottle block

--- freetype2501.rb
+++ freetype2501.rb
@@ -5,13 +5,6 @@ class Freetype2501 < Formula
   url 'http://downloads.sf.net/project/freetype/freetype2/2.5.0/freetype-2.5.0.1.tar.gz'
   sha1 '2d539b375688466a8e7dcc4260ab21003faab08c'
 
-  bottle do
-    # Included with X11 so no bottle needed before Mountain Lion.
-    revision 1
-    sha1 '54dad7e45cdbbdfa6065d13f9cbc497c94a94af5' => :mavericks
-    sha1 '9bfe424803590bccaaa39944b9b69e6ae7dbd548' => :mountain_lion
-  end
-
   keg_only :provided_pre_mountain_lion
 
   option :universal
